35a5d9bcc451a295c583c480b1f9c683f33c618b
jaimemin <jaimemin@gmail.com>
2023-05-04 00:49:15 +0900

    H2 DB 세팅 및 JPA 동작 확인

---
+++ src/main/java/com/tistory/jaimemin/springdatajpaproject/Member.java
@@ -0,0 +1,21 @@
+package com.tistory.jaimemin.springdatajpaproject;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Setter
+    private String username;
+
+}
