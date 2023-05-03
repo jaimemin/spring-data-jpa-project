ecaab97b3384eb8f11f511e4711f2cf257328585
jaimemin <jaimemin@gmail.com>
2023-05-03 23:51:01 +0900

    initial commit

---
+++ src/test/java/com/tistory/jaimemin/springdatajpaproject/SpringDataJpaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.jaimemin.springdatajpaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
