1bfa9c209fdaa48d3c662176fd8c2d3f6b1682fd
jaimemin <jaimemin@gmail.com>
2023-05-14 02:52:52 +0900

    QueryDSL 적용

--- src/main/java/com/tistory/jaimemin/springdatajpaproject/repository/OrderRepository.java
+++ src/main/java/com/tistory/jaimemin/springdatajpaproject/repository/OrderRepository.java
@@ -1,16 +1,16 @@
 package com.tistory.jaimemin.springdatajpaproject.repository;
 
-import com.tistory.jaimemin.springdatajpaproject.domain.Member;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.tistory.jaimemin.springdatajpaproject.domain.Order;
+import com.tistory.jaimemin.springdatajpaproject.domain.OrderStatus;
+import com.tistory.jaimemin.springdatajpaproject.domain.QMember;
+import com.tistory.jaimemin.springdatajpaproject.domain.QOrder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.*;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Repository
@@ -29,36 +29,23 @@ public class OrderRepository {
 
     /**
      * JPA Criteria (사실 실무에서는 QueryDSL이 보편적으로 쓰임)
+     * QueryDSL 적용
      *
      * @param orderSearch
      * @return
      */
     public List<Order> findAll(OrderSearch orderSearch) {
-        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-        CriteriaQuery<Order> criteriaQuery = criteriaBuilder.createQuery(Order.class);
-        Root<Order> o = criteriaQuery.from(Order.class);
-        Join<Order, Member> m = o.join("member", JoinType.INNER); //회원과 조인
-        List<Predicate> criteria = new ArrayList<>();
+        JPAQueryFactory query = new JPAQueryFactory(entityManager);
+        QOrder order = QOrder.order;
+        QMember member = QMember.member;
 
-        //주문 상태 검색
-        if (orderSearch.getOrderStatus() != null) {
-            Predicate status = criteriaBuilder.equal(o.get("status"),
-                    orderSearch.getOrderStatus());
-            criteria.add(status);
-        }
-
-        //회원 이름 검색
-        if (StringUtils.hasText(orderSearch.getMemberName())) {
-            Predicate name =
-                    criteriaBuilder.like(m.<String>get("name"), "%" +
-                            orderSearch.getMemberName() + "%");
-            criteria.add(name);
-        }
-
-        criteriaQuery.where(criteriaBuilder.and(criteria.toArray(new Predicate[criteria.size()])));
-        TypedQuery<Order> query = entityManager.createQuery(criteriaQuery).setMaxResults(1000); //최대 1000건
-
-        return query.getResultList();
+        return query
+                .select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()), nameLike(orderSearch.getMemberName()))
+                .limit(1000)
+                .fetch();
     }
 
     /**
@@ -109,4 +96,19 @@ public class OrderRepository {
                 .getResultList();
     }
 
+    private BooleanExpression statusEq(OrderStatus statusCond) {
+        if (statusCond == null) {
+            return null;
+        }
+
+        return QOrder.order.status.eq(statusCond);
+    }
+
+    private BooleanExpression nameLike(String memberName) {
+        if (!StringUtils.hasText(memberName)) {
+            return null;
+        }
+
+        return QMember.member.name.like(memberName);
+    }
 }
